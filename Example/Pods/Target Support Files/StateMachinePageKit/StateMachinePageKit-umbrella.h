acede5a1c1b789428ecaed1891006950f6c4d81a
lizitao <634376133@qq.com>
2022-11-28 18:05:17 +0800

    add StateMachinePageKit.h

--- Example/Pods/Target Support Files/StateMachinePageKit/StateMachinePageKit-umbrella.h
+++ Example/Pods/Target Support Files/StateMachinePageKit/StateMachinePageKit-umbrella.h
@@ -14,6 +14,7 @@
 #import "BaseStateViewController.h"
 #import "BaseViewState.h"
 #import "BaseZeroViewState.h"
+#import "StateMachinePageKit.h"
 
 FOUNDATION_EXPORT double StateMachinePageKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char StateMachinePageKitVersionString[];
---
+++ StateMachinePageKit/Classes/StateMachinePageKit.h
@@ -0,0 +1,16 @@
+//
+//  StateMachinePageKit.h
+//  MDStatePageKit
+//
+//  Created by Leon0206 on 2018/5/13.
+//
+
+#ifndef StateMachinePageKit_h
+#define StateMachinePageKit_h
+
+#import <StateMachinePageKit/BaseStateDelegate.h>
+#import <StateMachinePageKit/BaseStateViewController.h>
+#import <StateMachinePageKit/BaseViewState.h>
+#import <StateMachinePageKit/BaseZeroViewState.h>
+
+#endif /* StateMachinePageKit_h */
